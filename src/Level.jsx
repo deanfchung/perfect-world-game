b304de41f86fcc3fb82dbfbd7e01b6ee866ee5bc
dean <deanthechung@gmail.com>
2020-03-28 16:27:44 -0700

    level component

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 //This function is responsible for rendering the level and all
 //the entities in it.
 export default function Level(props) {
-
-  /*
-
-  Your code here
-
-  */
+  const map = useSelector(state => state.map)
+  console.log('state.map', map);
+  const grid = [];
+  for (let i = 0; i < map.length; i += 20) {
+    const tableRow = [];
+    for (let j = 0; j < 20; j++) {
+      let className;
+      if (map[i+j] === '@') {
+        className = 'player'
+      }
+      else if (map[i+j]==='#') {
+        className = 'wall'
+      }
+      else if (map[i+j]==='e') {
+        className = 'enemy'
+      }
+      tableRow.push(<td className = {className}key = {i+j} index = {i+j}>{map[i + j]}</td>)
+    }
+    grid.push(<tr key = {i/20}>{tableRow}</tr>);
+  }
 
   return (
     <table className="table">
       <tbody>
-      {/*
-
-      Your code here
-
-      */}
+        {grid}
       </tbody>
     </table>
+
   );
 };

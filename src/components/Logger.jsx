79f110892794c6834c116cb9a879b3b36ff1d791
dean <deanthechung@gmail.com>
2020-04-01 21:48:13 -0700

    insert drawer ref

--- src/components/Logger.jsx
+++ src/components/Logger.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Logger() {
-  let log = useSelector(state => state.log)
-  let turnHistory = [...log]
+    let log = useSelector(state => state.log)
+    let turnHistory = [...log]
+    const drawer = useRef()
 
-  useEffect(() => {
-    const drawer = document.querySelector('.drawer');
-    drawer.scrollTop = drawer.scrollHeight;
-  })
-  
-  if (turnHistory.length > 20) {
-      turnHistory = turnHistory.slice(Math.max(turnHistory.length - 20, 0))
+    useEffect(() => {
+        drawer.current.scrollTop = drawer.current.scrollHeight
+    })
+
+    if (turnHistory.length > 20) {
+        turnHistory = turnHistory.slice(Math.max(turnHistory.length - 20, 0))
     }
-  turnHistory = turnHistory.map((listItem, i) => {
-    return <li key = {i}>{listItem}</li>
-  })
+    turnHistory = turnHistory.map((listItem, i) => {
+        return <li key={i}>{listItem}</li>
+    })
 
-  return (
-    //feel free to change this however you'd like:
-    <div className="drawer text" id="logger">
-      <ul>{turnHistory}</ul>
-    </div>
-  );
-  }
+    return (
+        //feel free to change this however you'd like:
+        <div className='drawer text' ref={drawer} id='logger'>
+            <ul>{turnHistory}</ul>
+        </div>
+    )
+}
